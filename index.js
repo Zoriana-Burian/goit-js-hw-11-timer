e908cc8b5efe0bd28bfc22482247b1e9a546c143
Zoriana-Burian <ke7sha1991@gmail.com>
2020-10-29 13:57:16 +0200

    написання функцій для початку роботи таймера

---
+++ index.js
@@ -0,0 +1,43 @@
+const refs = {
+  days: document.querySelector('value[data-value="days"]'),
+  hours: document.querySelector('value[data-value="hours"]'),
+  mins: document.querySelector('value[data-value="mins"]'),
+  secs: document.querySelector('value[data-value="secs"]')
+}
+const timer = {
+  start() {
+    const startTime = Date.now();
+
+    setInterval(() => {
+      const curentTime = Date.now();
+      const deltaTime = curentTime - startTime;
+      const { days, hours, mins, secs } = getTimeComponent(deltaTime);
+
+    }, 1000)
+
+  }
+};
+
+timer.start();
+
+//new CountdownTimer({
+//    selector: '#timer-1',
+ //   targetDate: new Date('Jul 17, 2019'),
+ // });
+
+
+
+
+
+  function getTimeComponent(time) {
+    const days = pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+    const hours = pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
+    const mins = pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+    const secs = pad(Math.floor((time % (1000 * 60)) / 1000));
+   
+    return { days, hours, mins, secs };
+  }
+
+  function pad(value){
+     return String(value).padStart(2, '0');
+  }
